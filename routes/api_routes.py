6014d0bef50a24362f7d1ac1a09145a44b04cb68
Kcarlos_Dev <ca850075@gmail.com>
2025-12-27 16:12:35 -0300

    crindo rota para fiis

--- routes/api_routes.py
+++ routes/api_routes.py
@@ -1,7 +1,16 @@
 from flask import request,Blueprint,jsonify
-
+from service.fiis_web_scraping import get_fii
 tickers = Blueprint("tickers", __name__)
 
-@tickers.route("/", methods=["GET"])
-def get_tickers():
-    return jsonify({"message": "Hello, World!"})
+@tickers.route("/<ticker>", methods=["GET"])
+def get_tickers(ticker):
+    if not ticker:
+        return jsonify({"error": "Ticker não informado"}), 400
+    get_fii_data = get_fii(ticker)
+    try:
+        if not get_fii_data:
+            return jsonify({"error": "Ticker não encontrado"}), 404
+        else:
+            return jsonify(get_fii_data)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
